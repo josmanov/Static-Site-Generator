c2aef7523ee94ce4b0773e37057cdc079219a7f9
Jusuf Osmanovic <jusuf.osmanovic01@gmail.com>
2026-04-03 20:56:37 +0300

    create a text node

---
+++ public/src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode
+
+def main():
+    text = "This is some anchor text"
+    text_type = "link"
+    url = "https://www.boot.dev"
+    result = TextNode(text, text_type, url)
+    print(result)
+
+if __name__ == "__main__":
+    main()
---
+++ public/src/textnode.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class Bender(Enum):
+    AIR_BENDER = "air"
+    WATER_BENDER = "water"
+    EARTH_BENDER = "earth"
+    FIRE_BENDER = "fire"
+
+class TextNode():
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if self == other:
+            return True
+        return False
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
