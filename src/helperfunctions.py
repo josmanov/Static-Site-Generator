2730e595ab5ffbf97ebd9533c29d9810abbbb356
josmanov <jusuf.osmanovic01@gmail.com>
2026-04-09 23:04:58 +0300

    add image & link markdown extracting

--- src/helperfunctions.py
+++ src/helperfunctions.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -18,3 +19,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 else:
                     new_nodes.append(TextNode(part, text_type))
     return new_nodes
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
---
+++ src/test_extract_markdown.py
@@ -0,0 +1,33 @@
+import unittest
+
+from helperfunctions import extract_markdown_images, extract_markdown_links
+
+
+class TestTextNode(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+            " and also another ![image](https://i.imgur.com/5555jxJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png"),
+                              ("image", "https://i.imgur.com/5555jxJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with an [link](www.google.com)"
+        )
+        self.assertListEqual([("link", "www.google.com")], matches)
+
+        matches = extract_markdown_links(
+            "This is text with an [link](www.google.com)"
+            " and also another text with an [link](www.wikipedia.com)"
+        )
+        self.assertListEqual([("link", "www.google.com"),
+                              ("link", "www.wikipedia.com")], matches)
+
+    
