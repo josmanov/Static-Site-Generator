a78fba83b598eaf72dd48f95d3823df0d5ab15c0
josmanov <jusuf.osmanovic01@gmail.com>
2026-04-16 00:14:25 +0300

    add heading and code to html

--- src/block.py
+++ src/block.py
@@ -1,4 +1,12 @@
 from enum import Enum
+from htmlnode import ParentNode
+from textnode import text_node_to_html_node
+from splitnodes import text_to_textnodes
+from textnode import TextNode, TextType
+
+
+
+
 import re
 
 def markdown_to_blocks(markdown):
@@ -32,3 +40,90 @@ def block_to_block_type(block):
     elif all(re.match(r"^\d+\. ", line) for line in block.split("\n")):
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
+
+def markdown_to_html_node(markdown):
+    html_nodes = []
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type == BlockType.HEADING:
+            html_nodes.append(heading_to_html(block))
+        elif block_type == BlockType.CODE:
+            html_nodes.append(code_to_html(block))
+        elif block_type == BlockType.QUOTE:
+            html_nodes.append(quote_to_html(block))
+        elif block_type == BlockType.UNORDERED_LIST:
+            html_nodes.append(unordered_to_html(block))
+        elif block_type == BlockType.ORDERED_LIST:
+            html_nodes.append(ordered_to_html(block))
+        else:
+            html_nodes.append(paragraph_to_html(block))
+    return html_nodes
+
+def heading_to_html(text):
+    hashtag_count = 0
+    for char in text:
+        if char == "#":
+            hashtag_count += 1
+        else:
+            break
+    strip_text = text.strip("# ")
+    children = text_to_children(strip_text)
+    return ParentNode(f"h{hashtag_count}", children)
+
+def code_to_html(text):
+    strip_text = text.removeprefix("```").removesuffix("```")
+    node = TextNode(strip_text, TextType.CODE)
+    return ParentNode("pre", [text_node_to_html_node(node)])
+
+def quote_to_html(text):
+    hashtag_count = 0
+    for char in text:
+        if char == "#":
+            hashtag_count += 1
+        else:
+            break
+    strip_text = text.strip("# ")
+    children = text_to_children(strip_text)
+    return ParentNode(f"h{hashtag_count}", children)
+
+def unordered_to_html(text):
+    hashtag_count = 0
+    for char in text:
+        if char == "#":
+            hashtag_count += 1
+        else:
+            break
+    strip_text = text.strip("# ")
+    children = text_to_children(strip_text)
+    return ParentNode(f"h{hashtag_count}", children)
+
+def ordered_to_html(text):
+    hashtag_count = 0
+    for char in text:
+        if char == "#":
+            hashtag_count += 1
+        else:
+            break
+    strip_text = text.strip("# ")
+    children = text_to_children(strip_text)
+    return ParentNode(f"h{hashtag_count}", children)
+
+def paragraph_to_html(text):
+    hashtag_count = 0
+    for char in text:
+        if char == "#":
+            hashtag_count += 1
+        else:
+            break
+    strip_text = text.strip("# ")
+    children = text_to_children(strip_text)
+    return ParentNode(f"h{hashtag_count}", children)
+
+def text_to_children(text):
+    nodes = []
+    for text_node in text_to_textnodes(text):
+        nodes.append(text_node_to_html_node(text_node))
+    return nodes
+
+print(markdown_to_html_node("``` Hello World ```"))
